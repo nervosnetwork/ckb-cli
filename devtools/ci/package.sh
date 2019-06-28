1cb25f40cd8c466e91a2dcaf9a610d0c155b0f0b
Linfeng Qian <thewawar@gmail.com>
2019-06-28 12:35:18 +0800

    Update CI scripts

---
+++ devtools/ci/package.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu
+
+TRAVIS_TAG="${TRAVIS_TAG:-"$(git describe)"}"
+if [ -z "${REL_PKG:-}" ]; then
+  if [ "$(uname)" = Darwin ]; then
+    REL_PKG=x86_64-apple-darwin.zip
+  else
+    REL_PKG=x86_64-unknown-linux-gnu.tar.gz
+  fi
+fi
+
+PKG_NAME="ckb-cli_${TRAVIS_TAG}_${REL_PKG%%.*}"
+ARCHIVE_NAME="ckb-cli_${TRAVIS_TAG}_${REL_PKG}"
+
+rm -rf releases
+mkdir releases
+
+mkdir "releases/$PKG_NAME"
+cp "$1" "releases/$PKG_NAME"
+cp README.md CHANGELOG.md COPYING "releases/$PKG_NAME"
+
+pushd releases
+if [ "${REL_PKG#*.}" = "tar.gz" ]; then
+  tar -czf $PKG_NAME.tar.gz $PKG_NAME
+else
+  zip -r $PKG_NAME.zip $PKG_NAME
+fi
+if [ -n "${GPG_SIGNER:-}" ]; then
+  gpg -u "$GPG_SIGNER" -ab "$ARCHIVE_NAME"
+fi
+popd
