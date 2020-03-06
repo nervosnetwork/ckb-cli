d35284be0543d71e8e0cc37a03978c36b6a9d7b7
John Ericson <John.Ericson@Obsidian.Systems>
2020-03-06 14:29:07 -0500

    Cargo fmt

--- ckb-sdk/src/wallet/keystore/interface.rs
+++ ckb-sdk/src/wallet/keystore/interface.rs
@@ -280,7 +280,9 @@ pub const MANDATORY_PREFIX: &[ChildNumber] = &[
 ];
 
 pub fn is_valid_derivation_path(path: &[ChildNumber]) -> bool {
-    path.iter().map(Some).chain(std::iter::repeat(None))
+    path.iter()
+        .map(Some)
+        .chain(std::iter::repeat(None))
         .zip(MANDATORY_PREFIX.iter())
         .all(|(x, y)| x == Some(y))
 }
